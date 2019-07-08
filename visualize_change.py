35cf635d29a0e8f71e5d8a1f3fe97eba943b729d
jackoaw <jackoaw@vt.edu>
2019-07-07 21:12:49 -0400

    fixed logic for program, should have done it against every day, not just every 3rd day

--- visualize_change.py
+++ visualize_change.py
@@ -25,25 +25,27 @@ with open (readfile, "r") as f:
 
 allnums = 0 
 
-prev_close = 0
-for i in range(1,len(data), DAYS):
+# prev_close = 0
+for i in range(1, len(data)):
 	line = data[i]
-	cols = line.split(",")
-	close_price = float(cols[4])
-	if prev_close is 0:
-		prev_close = close_price
-		continue
+	try:
+		comp_line = data[i + DAYS]
+	except:
+		#There is no more data to compare against if this is reached
+		break
+	cols1 = line.split(",")
+	close_price1 = float(cols1[4])
+	cols2 = comp_line.split(",")
+	close_price2 = float(cols2[4])
+	dif = close_price2 - close_price1
+	poc = dif/close_price2 *100 #percent of change
+	poc = float("%.1f"%poc)
+	if poc in change_map.keys():
+		change_map[poc] += 1
 	else:
-		dif = close_price - prev_close
-		prev_close = close_price
-		poc = dif/close_price *100 #percent of change
-		poc = float("%.1f"%poc)
-		if poc in change_map.keys():
-			change_map[poc] += 1
-		else:
-		 	change_map[poc] = 1
-		change_list.append(poc)
-		allnums +=1
+	 	change_map[poc] = 1
+	change_list.append(poc)
+	allnums +=1
 
 for key in sorted(change_map.keys()):
     print("%s%%: %.2f%%" % (key, change_map[key]*1.0/allnums*100))
